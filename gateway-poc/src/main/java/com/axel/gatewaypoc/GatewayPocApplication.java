fb6bc6558a2710ca9e4a688d45849701c7cff1d1
Axel Aviloff <53865142+axelaviloff@users.noreply.github.com>
2021-06-29 13:36:44 -0300

    Add initial files

---
+++ gateway-poc/src/main/java/com/axel/gatewaypoc/GatewayPocApplication.java
@@ -0,0 +1,13 @@
+package com.axel.gatewaypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayPocApplication.class, args);
+	}
+
+}
